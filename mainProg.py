e7d40f588ae5d2143faadd9b9533d0d2cf30adc4
JTcgh <74654976+d0phyte@users.noreply.github.com>
2023-12-12 00:42:36 +0000

    Add files via upload

---
+++ mainProg.py
@@ -0,0 +1,16 @@
+#number = input("Insert number")
+
+x = input("Insert number: ")
+
+number = str(x)
+
+firstDigit = number[0]
+
+secondDigit = number[1]
+
+#print(secondDigit)
+
+mathFinal = int(firstDigit) + int(secondDigit)
+
+print(mathFinal)
+
